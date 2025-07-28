2ee3815860155dbc452ccf1938812608508491f4
tsun222 <tsun3126@gmail.com>
2025-07-28 19:00:55 -0400

    Feat: added profiles and ability to edit profiles

--- myapp/models.py
+++ myapp/models.py
@@ -2,9 +2,11 @@ from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from myapp import db
 from myapp import login
+from hashlib import md5
 
 
 @login.user_loader
@@ -12,13 +14,15 @@ def load_user(id):
     return db.session.get(User, int(id))
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
     posts: so.WriteOnlyMapped["Post"] = so.relationship(back_populates="author")
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
 
     def __repr__(self):
         return "<User {}>".format(self.username)
@@ -30,6 +34,10 @@ class User(db.Model):
         if self.password_hash is None:
             return False
         return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
 
 class Post(db.Model):
